a9fc2eba1c1502231a8d97998db3de25353fa8a6
bryankenote <brykenote@gmail.com>
2022-06-25 19:15:13 -0700

    add unassign device and fork

---
+++ sql/sprocs/sp_unassign_device.sql
@@ -0,0 +1,42 @@
+USE [wms]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[sp_unassign_device]
+    @paramWhId INT,
+	@paramUserCode NVARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE
+	@ERROR NVARCHAR(200),
+	@userId int
+
+	-- get user id
+	SELECT @userId = id
+	FROM [t_user]
+	WHERE code = @paramUserCode
+    AND wh_id = @paramWhId
+
+	IF @userId IS NULL
+	BEGIN
+		SELECT @ERROR = 'user does not exist'
+		SELECT @ERROR
+		RETURN
+	END
+
+	-- assign device to user
+	UPDATE dbo.[t_user]
+	SET device_id = NULL
+	WHERE id = @userId
+    AND wh_id = @paramWhId
+
+	SELECT @ERROR
+END
+GO
