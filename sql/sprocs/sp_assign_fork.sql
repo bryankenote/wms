3d92e0af35a6fe5889de32ff84fb81770b10c01d
bryankenote <brykenote@gmail.com>
2022-06-25 19:15:22 -0700

    improve assign fork errors

--- sql/sprocs/sp_assign_fork.sql
+++ sql/sprocs/sp_assign_fork.sql
@@ -7,7 +7,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER PROCEDURE [dbo].[sp_assign_fork]
+CREATE PROCEDURE [dbo].[sp_assign_fork]
     @paramWhId INT,
 	@paramUserCode NVARCHAR(50),   
     @paramFork NVARCHAR(50)
@@ -18,7 +18,8 @@ BEGIN
 	DECLARE
 	@ERROR NVARCHAR(200),
 	@userId int,
-	@forkId int
+	@forkId int,
+	@assignedUser int
 
 	-- get user id
 	SELECT @userId = id
@@ -34,19 +35,26 @@ BEGIN
 	END
 
 	-- get fork id
-	SELECT @forkId = loc.id
+	SELECT @forkId = loc.id,
+			@assignedUser = usr.id
 	FROM [t_location] loc
     LEFT JOIN [t_user] usr
         ON usr.fork_id = loc.id
         AND usr.wh_id = loc.wh_id
 	WHERE [type] = 'F'
 	AND loc.code = @paramFork
-    AND usr.id IS NULL
     AND loc.wh_id = @paramWhId
 	
 	IF @forkId IS NULL
 	BEGIN
 		SELECT @ERROR = 'fork does not exist'
+		SELECT @ERROR
+		RETURN
+	END
+
+	IF @assignedUser IS NOT NULL
+	BEGIN
+		SELECT @ERROR = 'fork is assigned to another user'
 		SELECT @ERROR
 		RETURN
 	END
